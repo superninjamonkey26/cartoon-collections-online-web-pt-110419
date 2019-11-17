6f119c275ce6d11336b7368ed2295fc82dfb4c38
Monkey Mendel <stephanie.mendel26@gmail.com>
2019-11-17 00:43:08 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -8,8 +8,10 @@ def summon_captain_planet(array)
   end
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(array)
+  if array.length > 4
+    true
+  end
 end
 
 def find_the_cheese# code an argument here
