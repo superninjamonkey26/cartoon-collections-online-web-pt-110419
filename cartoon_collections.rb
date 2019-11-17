051c2b0d8c421c2ad8f65ac80dcf554aa2c31838
Monkey Mendel <stephanie.mendel26@gmail.com>
2019-11-17 01:11:18 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -15,7 +15,6 @@ def long_planeteer_calls(array)
 end
 
 def find_the_cheese(array)
-  # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
-  array.include?("cheddar")
+  array.find do |cheese|
 end
