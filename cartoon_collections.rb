4181cfd99e57a89351e0ad90a42ac7f28601bcbf
Monkey Mendel <stephanie.mendel26@gmail.com>
2019-11-17 00:34:22 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,9 +1,11 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(array)
+  index = 0
+  array.each_with_index {|name, index| puts "#{index+1}. #{name}"}
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(array)
+  array.collect do |name| name.capitalize + "!"
+  end
 end
 
 def long_planeteer_calls# code an argument here
